05e888b402ba4e6465f74df87f17a294b01be0c9
dinob336 <dino.baljak@gmail.com>
2022-06-21 12:24:12 +0200

    Added player + food
    Added player + food and basic mechanics

---
+++ Assets/Scripts/Food.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+    public GameObject foodPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+        RandomPosition();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RandomPosition()
+    {
+        int xPos = Random.Range(-9, 9);
+        int zPos = Random.Range(-9, 9);
+
+        transform.position = new Vector3(xPos, 0, zPos);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Instantiate(foodPrefab);
+            RandomPosition();
+        }
+       
+    }
+    
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    public GameObject player;
+    public Vector3 playerPos;
+    public Vector3 direction;
+
+    public List<Transform> segments;
+    public Transform segmentPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        segments = new List<Transform>();
+        segments.Add(transform);
+
+        
+        direction = Vector3.forward;
+        // StartCoroutine(Move());
+
+        
+
+        
+        
+
+    }
+    private void Update()
+    {
+        Movement();
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        for(int i = segments.Count -1; i >0; i--)
+        {
+            segments[i].position = segments[i - 1].position;
+        }
+        transform.position = new Vector3(
+            Mathf.Round(transform.position.x) + direction.x,
+                0,
+            Mathf.Round(transform.position.z + direction.z)
+
+            );
+        
+    }
+
+    private void Movement()
+    {
+        playerPos = transform.position;
+        if (Input.GetKeyDown(KeyCode.UpArrow)) { direction = Vector3.forward; }
+        if (Input.GetKeyDown(KeyCode.DownArrow)) { direction = Vector3.back; }
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) { direction = Vector3.left; }
+        if (Input.GetKeyDown(KeyCode.RightArrow)) { direction = Vector3.right; }
+
+    }
+    
+    
+
+    private void IncreaseSize()
+    {
+        Transform segment = Instantiate(segmentPrefab);
+        segment.position = segments[segments.Count - 1].position;
+
+        segments.Add(segment);
+        
+    }
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+        IncreaseSize();
+        //Time.fixedDeltaTime -= 0.1f;
+        
+    }
+
+
+}
